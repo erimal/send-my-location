a3ceb802c01d1a0f63ba663ab37676096ff7e78b
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-08-30 18:19:42 +0000

    Insert form data into database.

--- www/handler.php
+++ www/handler.php
@@ -1,9 +1,20 @@
 <?php
 
-$phone = $_POST['phone'];
-$longitude = $_POST['longitude'];
-$latitude = $_POST['latitude'];
+$db = "sqlite:../database.sqlite";
+$pdo = new PDO($db);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo "Phone: $phone<br>";
-echo "Longitude: $longitude<br>";
-echo "Latitude: $latitude<br>";
+try {
+    $stmt = $pdo->prepare(
+        "insert into orders(phone, longitude, latitude) values(?, ?, ?)"
+    );
+
+    $stmt->bindValue(1, $_POST['phone']);
+    $stmt->bindValue(2, $_POST['longitude']);
+    $stmt->bindValue(3, $_POST['latitude']);
+    $stmt->execute();
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+echo "Thank you for your business!";
