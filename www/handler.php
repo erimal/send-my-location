07b1518e3349067e2bdc4c382492841f7186c26f
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-08-30 16:53:23 +0000

    Initial form handler code.

---
+++ www/handler.php
@@ -0,0 +1,9 @@
+<?php
+
+$phone = $_POST['phone'];
+$longitude = $_POST['longitude'];
+$latitude = $_POST['latitude'];
+
+echo "Phone: $phone<br>";
+echo "Longitude: $longitude<br>";
+echo "Latitude: $latitude<br>";
