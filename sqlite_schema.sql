e819233b13efbf79ac9aff156f733000a9a502af
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-08-30 19:33:30 +0000

    Added timestamp column to orders table.

--- sqlite_schema.sql
+++ sqlite_schema.sql
@@ -1,5 +1,6 @@
 create table orders(
     id integer primary key,
+    timestamp date default (datetime('now', 'localtime')),
     phone varchar(20) not null,
     longitude varchar(8),
     latitude varchar(8)
