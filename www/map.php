f7453a97c8139a4b953b576a225f719574494344
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-08-31 00:21:39 +0000

    Place markers on the map.

--- www/map.php
+++ www/map.php
@@ -21,7 +21,7 @@
 <div id="mapid"></div>
 
 <script type="text/javascript">
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([5.55, -0.2167], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -29,6 +29,20 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoibG9yZW56b2NhYnJpbmkiLCJhIjoiY2o2emdvdWV6Mml6dTJxbjcxbmg0Y2JjNyJ9.4XVM2qJTEh4c4GTmeWH-sA'
 }).addTo(mymap);
+
+<?php
+$db = "sqlite:../database.sqlite";
+$pdo = new PDO($db);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$result = $pdo->query("select * from orders");
+foreach ($result as $row) {
+    $phone = $row['phone'];
+    $longitude = $row['longitude'];
+    $latitude = $row['latitude'];
+    echo "var marker = L.marker([$latitude, $longitude]).addTo(mymap);\n";
+    echo "marker.bindPopup('$phone').openPopup();\n";
+}
+?>
 </script>
 </body>
 </html>
