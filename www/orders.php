df59b7d2e97f167ac8dbd0285a9a7adf26e6adf3
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-09-06 22:27:58 +0000

    Added reverse geocoding column to orders table.

--- www/orders.php
+++ www/orders.php
@@ -2,6 +2,17 @@
 
 include_once('db.inc.php');
 
+function get_address($lat, $long) {
+    $url = "http://maps.googleapis.com/maps/api/geocode/json?latlng="
+        . "$lat,$long";
+    $file = file_get_contents($url);
+    $json = json_decode($file, true);
+
+    $route = $json['results'][0]['address_components'][0]['long_name'];
+    $sublocality = $json['results'][0]['address_components'][1]['long_name'];
+    return "$route, $sublocality";
+}
+
 if (array_key_exists("id", $_GET)) {
     try {
         //$stmt = $pdo->prepare("delete from orders where id = ?");
@@ -26,6 +37,7 @@ if (array_key_exists("id", $_GET)) {
 <table>
 <tr>
 <th>Phone Number</th>
+<th>Location</th>
 <th>Order Date and Time</th>
 <th>Operations</th>
 <?php
@@ -34,6 +46,7 @@ try {
     foreach ($result as $row) { ?>
         <tr>
         <td><?= $row['phone'] ?></td>
+        <td><?= get_address($row['latitude'], $row['longitude']) ?></td>
         <td><?= $row['timestamp'] ?></td>
         <td><a href="/orders.php?id=<?= $row['id'] ?>">Close</a></td>
         </tr>
