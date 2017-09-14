978e4c12a0b964da2e44cc9ce4484875fa63e7a9
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-09-14 21:54:23 +0000

    Send SMS.

--- www/handler.php
+++ www/handler.php
@@ -1,6 +1,8 @@
 <?php
 
 include_once('db.inc.php');
+include_once('lib.inc.php');
+require_once('vendor/telerivet-php-client-master/telerivet.php');
 
 try {
     $stmt = $pdo->prepare(
@@ -14,5 +16,13 @@ try {
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
+
+$address = get_address();
+$tr = new Telerivet_API('API Key');
+$project = $tr->initProjectById('Project ID');
+$contact = $project->getOrCreateContact(array('vars' => array(
+	"L_Street" => $address[0], 
+	"L_Area" => $address[1])
+));
 
 echo "Thank you for your business!";
