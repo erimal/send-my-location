d831ab536819c7ede18411bd61e0432bce3bb5ec
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-09-14 21:35:25 +0000

    Return of get_address() should be array.

--- www/db.inc.php
+++ www/db.inc.php
@@ -1,8 +1,13 @@
 <?php
 
 try {
-    $dbname = "sqlite:../database.sqlite";
-    $pdo = new PDO($dbname);
+    //$dbname = "sqlite:../database.sqlite";
+    $dbname = "restaurant";
+    $user = "root";
+    $password = "m00kaw";
+    $host = "localhost";
+    //$pdo = new PDO($dbname);
+    $pdo = new PDO("mysql:dbname=$dbname;host=$host", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo $e->getMessage();
